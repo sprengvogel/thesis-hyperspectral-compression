19e4b23ee8bce5d1e990bb808f48d9dc1cf952ba
sprengvogel <niklasspreng@gmx.de>
2023-05-15 19:37:50 +0000

    Added real interpolation implementations and fixed bugs

--- Code/train_fast_combined_model.py
+++ Code/train_fast_combined_model.py
@@ -18,7 +18,7 @@ def load_outer_model(artifact_id):
         artifact_id, type='model')
     artifact_dir = artifact.download()
     inner_model = models.Fast1DConvModel(
-        nChannels=p.CHANNELS, bottleneck_size=13, H=128, W=128)
+        nChannels=p.CHANNELS, bottleneck_size=26, H=128, W=128)
     conv_model = models.LitAutoEncoder(inner_model, lr=p.LR)
     conv_model.load_from_checkpoint(
         artifact_dir+"/model.ckpt", model=inner_model)
@@ -34,13 +34,17 @@ def load_outer_model(artifact_id):
 if __name__ == "__main__":
     # Old standard (latent 13)
     # model_id = "37bkqy6m:v0"
+    # Bitrate comp latent 26
+    model_id = "2lnyd5cg:v0"
     # Bitrate comp latent 13
-    model_id = "s1w3vien:v0"
+    # model_id = "s1w3vien:v0"
+    # Bitrate comp latent 6
+    # model_id = "3dyoflur:v0"
     outer_model = load_outer_model(
         f"niklas-sprengel/MastersThesis/model-{model_id}")
 
     model = models.LitAutoEncoder(models.FastCombinedModel(
-        nChannels=p.CHANNELS, bottleneck_size=13, H=128, W=128, outerModel=outer_model),
+        nChannels=p.CHANNELS, bottleneck_size=26, H=128, W=128, outerModel=outer_model),
         lr=p.LR, loss=metrics.DualMSELoss(p.DUAL_MSE_LOSS_LMBDA), model_type=models.ModelType.CONV1D_AND_2D)
     data.train_and_test(
         model, use_early_stopping=p.USE_EARLY_STOPPING, batch_size=4)
--- Code/train_interpolation_baseline.py
+++ Code/train_interpolation_baseline.py
@@ -10,6 +10,8 @@ import wandb
 import torch
 from tqdm import tqdm
 import math
+import argparse
+import scipy.interpolate as ip
 
 
 def convertVNIRImageToRGB(hyperspectral_image: torch.Tensor):
@@ -34,27 +36,57 @@ def compute_split_points(big_channels, small_channels):
     """
     Computes the split points used for interpolation and restoration
     """
-    # We need one extra split point, because both start and end are included
-    return np.linspace(start=0, stop=big_channels-1, num=small_channels+1, endpoint=True, dtype=int)
+    return np.linspace(start=0, stop=big_channels-1, num=small_channels, endpoint=True, dtype=int)
 
 
-def interpolate_image(img_batch, n_output_channels):
+def compress_image_with_averaging(img_batch, n_output_channels):
     """
-    Interpolate image channels in a naive manner
+    Compress image by averaging values between split points
+    Input: Img batch with dimensions (B, C, H, W)
+    Output: Compressed img batch with dimensions (B, n_output_channels, H, W)
+    """
+    in_channels = img_batch.shape[1]
+    # We need one extra split point, because both start and end are included
+    split_points = compute_split_points(
+        big_channels=in_channels, small_channels=n_output_channels+1)
+    interpolated_values = [np.average(img_batch[:, split_points[i]:split_points[i+1], :, :], axis=1)
+                           for i in range(len(split_points)-1)]
+    return np.stack(interpolated_values, axis=1)
+
+
+def decode_averaged_image(img_batch, n_output_channels=p.CHANNELS):
+    """
+    'Decodes' interpolated image by setting the value of the interpolated channel for all channels it is interpolated from
+    """
+    in_channels = img_batch.shape[1]
+    result = np.empty(
+        (img_batch.shape[0], n_output_channels, img_batch.shape[2], img_batch.shape[3]))
+    # We need one extra split point, because both start and end are included
+    split_points = compute_split_points(
+        big_channels=n_output_channels, small_channels=in_channels+1)
+    for i in range(len(split_points)-1):
+        split_point = split_points[i]
+        next_point = split_points[i+1]
+        result[:, split_point:next_point, :, :] = np.expand_dims(
+            img_batch[:, i, :, :], 1)
+    return result
+
+
+def encode_general_interpolation(img_batch, n_output_channels):
+    """
+    Encodes by throwing away all points except for @n_output_channels number of points, chosen equidistant
     Input: Img batch with dimensions (B, C, H, W)
     Output: Interpolated img batch with dimensions (B, n_output_channels, H, W)
     """
     in_channels = img_batch.shape[1]
     split_points = compute_split_points(
         big_channels=in_channels, small_channels=n_output_channels)
-    interpolated_values = [np.average(img_batch[:, split_points[i]:split_points[i+1], :, :], axis=1)
-                           for i in range(len(split_points)-1)]
-    return np.stack(interpolated_values, axis=1)
+    return img_batch[:, split_points, :, :]
 
 
-def decode_interpolated_image(img_batch, n_output_channels=p.CHANNELS):
+def decode_linear_interpolation(img_batch, n_output_channels=p.CHANNELS):
     """
-    'Decodes' interpolated image by setting the value of the interpolated channel for all channels it is interpolated from
+    Performs linear interpolation on the image batch
     """
     in_channels = img_batch.shape[1]
     result = np.empty(
@@ -64,16 +96,34 @@ def decode_interpolated_image(img_batch, n_output_channels=p.CHANNELS):
     for i in range(len(split_points)-1):
         split_point = split_points[i]
         next_point = split_points[i+1]
-        result[:, split_point:next_point, :, :] = np.expand_dims(
-            img_batch[:, i, :, :], 1)
+        delta = img_batch[:, i+1, :, :] - img_batch[:, i, :, :]
+        gradient = delta / (next_point-split_point)
+        for j in range(next_point-split_point):
+            result[:, split_point+j, :, :] = img_batch[:, i, :, :] + j*gradient
     return result
 
 
-if __name__ == "__main__":
-    batch_size = 128
-    latent_channels = 56
+def decode_general_interpolation(img_batch, kind: str, n_output_channels=p.CHANNELS):
+    """
+    Performs different kinds of interpolation on the image batch
+    """
+    in_channels = img_batch.shape[1]
+    result = np.empty(
+        (img_batch.shape[0], n_output_channels, img_batch.shape[2], img_batch.shape[3]))
+    split_points = compute_split_points(
+        big_channels=n_output_channels, small_channels=in_channels)
+    for b in range(img_batch.shape[0]):
+        for x in range(img_batch.shape[2]):
+            for y in range(img_batch.shape[3]):
+                f = ip.interp1d(
+                    split_points, img_batch[b, :, x, y], kind=kind)
+                result[b, :, x, y] = f(np.arange(n_output_channels))
+    return result
+
+
+def main(batch_size: int, latent_channels: int, interpolation_mode: str):
     run = wandb.init(project="MastersThesis", tags=[
-                     "baseline", "interpolation"], name=f"InterpolationLatent{latent_channels}")
+                     "baseline", "interpolation"], name=f"{interpolation_mode.capitalize()}InterpolationLatent{latent_channels}")
     dataset = data.HySpecNet11k(
         p.DATA_FOLDER_HYSPECNET, mode="easy", split="test")
     dataloader = DataLoader(dataset, batch_size=batch_size,
@@ -81,9 +131,22 @@ if __name__ == "__main__":
     mse = []
     psnr = []
     for batch_idx, batch in enumerate(tqdm(dataloader)):
-        latent = interpolate_image(
-            batch.numpy(), n_output_channels=latent_channels)
-        rec = decode_interpolated_image(latent)
+        if interpolation_mode == "average":
+            latent = compress_image_with_averaging(
+                batch.numpy(), n_output_channels=latent_channels)
+            rec = decode_averaged_image(latent)
+        elif interpolation_mode == "linear":
+            latent = encode_general_interpolation(
+                batch.numpy(), n_output_channels=latent_channels)
+            rec = decode_linear_interpolation(latent)
+        elif interpolation_mode == "linear2":
+            latent = encode_general_interpolation(
+                batch.numpy(), n_output_channels=latent_channels)
+            rec = decode_general_interpolation(latent, kind="linear")
+        elif interpolation_mode == "cubic":
+            latent = encode_general_interpolation(
+                batch.numpy(), n_output_channels=latent_channels)
+            rec = decode_general_interpolation(latent, kind="cubic")
         rec = torch.from_numpy(rec)
         assert latent.shape[1:] == (latent_channels, 128, 128)
         assert rec.shape == batch.shape
@@ -96,3 +159,32 @@ if __name__ == "__main__":
                 {f"test_images/latent{batch_idx}": [wandb.Image(latent[0, i, :, :]) for i in range(min(latent_channels, 13))]})
     run.log({"test_loss/loss": np.mean(mse), "test_loss/mse": np.mean(mse),
             "test_metrics/psnr": np.mean(psnr), "test_metrics/bpppc": 32*latent_channels/p.CHANNELS})
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='Evaluate interpolation methods on test set')
+    parser.add_argument(
+        "-b",
+        "--batchsize",
+        type=int,
+        default=128,
+        help="Batch size",
+    )
+    parser.add_argument(
+        "-c",
+        '--channels',
+        default=13,
+        type=int,
+        help='Number of desired latent channels')
+    parser.add_argument(
+        "-m",
+        "--mode",
+        default="linear",
+        type=str,
+        choices=["linear", "average", "linear2", "cubic"],
+        help="Interpolation algorithm (default: %(default)s)",
+    )
+
+    parse_args = parser.parse_args()
+    main(parse_args.batchsize, parse_args.channels, parse_args.mode)
